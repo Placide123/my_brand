7a268802e2e0b6b2a534c1c8537dbcf5d312d4dc
Placide123 <placidetwiringiyimana@outlook.com>
2023-03-13 15:47:10 +0200

    add my work

--- my_brand/src/component/Mywork.js
+++ my_brand/src/component/Mywork.js
@@ -2,11 +2,57 @@
 export default function Mywork() {
 
     return (
-    <>
-    <div className="flex justify-center bg-primary h-12 p-2 ">
-        <h1 className="p-2">Designed by Placide TWIRINGIYIMANA</h1>
-    </div>
-    </>
+        <>
+            <div className="flex flex-col justify-center bg-primary h-[100vh] p-2 ">
+                <h1 className="p-2">My work</h1>
+                <div className="flex lg:flex-row  m-10 border-2 border-lime-300">
+                    <div>
+                        <img
+                            src="https://images.unsplash.com/photo-1670272504471-61a632484750?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
+                            alt="a person"
+                            className="w-110 h-96 object-cover mr-4 rounded-t-lg"
+                        />
+                        <h2 className="text-4xl mb-2 flex justify-center font-bold">barefoot nomad</h2>
+                        <p>
+                            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard
+                            dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                            It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+                            It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,
+                            and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                        </p>
+                    </div>
+                    <div>
+                    <img
+                            src="https://images.unsplash.com/photo-1670272504471-61a632484750?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
+                            alt="a person"
+                            className="w-110 h-96 object-cover mr-4 rounded-t-lg"
+                        />
+                        <h2 className="text-4xl mb-2 flex justify-center font-bold">barefoot nomad</h2>
+                        <p>
+                            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard
+                            dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                            It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+                            It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,
+                            and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                        </p>
+                    </div>
+                    <div>
+                    <img
+                            src="https://images.unsplash.com/photo-1670272504471-61a632484750?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
+                            alt="a person"
+                            className="w-110 h-96 object-cover mr-4 rounded-t-lg"
+                        />
+                        <h2 className="text-4xl mb-2 flex justify-center font-bold">barefoot nomad</h2>
+                        <p>
+                            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard
+                            dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                            It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+                            It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,
+                            and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </>
     );
-  }
-  
+}
--- my_brand/src/pages/Homepage.js
+++ my_brand/src/pages/Homepage.js
@@ -1,5 +1,5 @@
 import Placide from "../images/placidebio.png";
-import { FaGithub, FaInstagram } from 'react-icons/fa';
+import { FaGithub, FaInstagram, FaTwitter } from 'react-icons/fa';
 import { FaLinkedin } from 'react-icons/fa';
 
 export default function Homepage() {
@@ -15,15 +15,16 @@ export default function Homepage() {
           back-end development. I have a bachelor's degree in Computer
           engineering. My primary goal in the global IT sector is to provide the most perfect product possible.
         </p>
-        <div className="flex ml-8 mb-3">
-          <a href="https://www.instagram.com/yourusername/" target="_blank" rel="noopener noreferrer" className="mr-3 text-lime-400 text-2xl"><FaInstagram/></a>
-          <a href="https://www.linkedin.com/in/yourusername/" target="_blank" rel="noopener noreferrer" className="mr-3 text-lime-400 text-2xl"><FaLinkedin/></a>
-          <a href="https://github.com/yourusername" target="_blank" rel="noopener noreferrer" className="text-lime-400 text-2xl"><FaGithub/></a>
+        <div className="flex ml-8 gap-5 mb-3">
+          <a href="https://www.instagram.com/yourusername/" target="_blank" rel="noopener noreferrer" className="text-primary  text-2xl"><FaInstagram/></a>
+          <a href="https://www.linkedin.com/in/yourusername/" target="_blank" rel="noopener noreferrer" className=" text-primary  text-2xl"><FaLinkedin/></a>
+          <a href="https://github.com/yourusername" target="_blank" rel="noopener noreferrer" className="text-primary text-2xl"><FaGithub/></a>
+          <a href="https://github.com/yourusername" target="_blank" rel="noopener noreferrer" className="text-primary text-2xl"><FaTwitter/></a>
         </div>
-        <button className="ml-8 p-2 bg-primary font-bold rounded">Let's talk</button>
+        <button className="ml-8 p-2 bg-primary font-bold rounded w-1/5">Let's talk</button>
       </div>
       <div className="flex justify-center items-center lg:w-1/2">
-        <img src={Placide} alt="bio" className="object-cover h-2/4 w-3/5 rounded lg:rounded-full lg:object-contain lg:h-full" />
+        <img src={Placide} alt="bio" className="object-cover h-2/4 w-2/5 rounded lg:rounded-full lg:object-contain lg:h-full" />
       </div>
     </div>
   );
