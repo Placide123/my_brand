14faa27f80c5bfb6702865b19102b3d415c9fcda
Placide123 <placidetwiringiyimana@outlook.com>
2023-03-18 12:44:23 +0200

    work on navigation bar

--- my_brand/src/component/Navigation.js
+++ my_brand/src/component/Navigation.js
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import { NavLink } from "react-router-dom";
+import { FiMenu } from 'react-icons/fi'
+
 
 export default function Navigation() {
   const [isOpen, setIsOpen] = useState(false);
@@ -9,31 +11,41 @@ export default function Navigation() {
   };
 
   return (
-    <nav className="flex flex-wrap justify-between border-2 border-blue-600 p-2">
-      <div>
-        <h3 className="pl-3">TWC</h3>
-      </div>
-      <div>
-        <ul className="flex gap-2">
-          <li>Home</li>
-          <li>Blog</li>
-        </ul>
-      </div>
-      <div className="flex items-center gap-2">
-        <button
-          className="block sm:hidden border border-blue-600 p-1 rounded"
-          onClick={toggleMenu}
-        >
-          {isOpen ? "Close" : "Menu"}
+
+    <nav class="bg-primary border-gray-200 px-2 md:px-4 py-2.5 rounded dark:bg-gray-900">
+      <div class="container flex flex-wrap items-center justify-between mx-auto">
+        <h1 class="flex items-center">
+          TWC
+        </h1>
+        <button data-collapse-toggle="navbar-default" type="button" class="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-gray-200 " aria-controls="navbar-default" aria-expanded="false" onClick={() => setIsOpen(!isOpen)}>
+          <span class="sr-only">Open main menu</span>
+          {/* <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg> */}
+          <FiMenu />
+
         </button>
-        <ul
-          className={`${isOpen ? "block " : "hidden"
-            } sm:flex sm:items-center sm:w-auto w-full`}
-        >
-          <li className="pr-3 pl-3 border-2 border-lime-300 mr-2">Resume</li>
-          <li className="hover:bg-lime-300 p-2 hover:rounded"><NavLink to="/login"><span>LOGIN</span></NavLink></li>
-        </ul>
-      </div>
+        <div className={`${isOpen ? '' : 'hidden'
+          } w-full block flex-grow lg:block  lg:w-auto md:block md:w-auto`}
+          id="navbar-default">
+          <ul class="flex flex-col p-4 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white md:justify-end ">
+            <li>
+              <a href="#" class="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0" aria-current="page">Home</a>
+            </li>
+            <li>
+              <a href="#" class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 ">About</a>
+            </li>
+            <li>
+              <a href="#" class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 ">Service</a>
+            </li>
+            <li>
+              <a href="#" class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0  ">LOGIN</a>
+            </li>
+            <li>
+              <a href="#" class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 ">Resume</a>
+            </li>
+          </ul>
+        </div>
+        </div>
     </nav>
+
   );
 }
