648a29bdef337be8e99ad948cec641c4d14a7857
Placide123 <placidetwiringiyimana@outlook.com>
2023-03-19 19:13:26 +0200

    responsiveness login and signup

--- my_brand/src/pages/Login.js
+++ my_brand/src/pages/Login.js
@@ -2,23 +2,24 @@ import { NavLink } from "react-router-dom";
 
 export default function Login() {
     return (
-        <div className="bg-[url('https://images.unsplash.com/photo-1497864149936-d3163f0c0f4b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1169&q=80')] bg-cover">
-            <div className="flex flex-col items-center justify-center px-12 py-24 mx-auto md:h-screen lg:py-0 border-2 border-white">
-            <h1 className="block mb-2 text-sm font-medium text-white text-xl ">Sign in to your account</h1>
-                <form className="space-y-8 md:space-y-6  h-96 w-96 p-4 mt-4">
+        <div className="bg-gray-100">
+            <div className="flex flex-col justify-center h-screen items-center md:space-x-4 md:h-screen lg:py-0 border-2 border-primary">
+                <h1 className="flex justify-center items-center mb-6">TWC</h1>
+                <h1 className="block mb-2 text-sm font-medium text-white md:text-xl ">Sign in to your account</h1>
+                <form className="space-y-8 md:space-y-6  h-96 w-10/12 md:w-1/3 lg:w-1/3 xl:w-1/4 p-4 mt-4 shadow-2xl rounded">
                     <div className="flex flex-col">
                         <label className="block mb-2 text-sm font-medium text-white ">Email</label>
-                        <input type="text" placeholder="email" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"></input>
+                        <input type="text" placeholder="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"></input>
                     </div>
                     <div className="flex flex-col">
                         <label className="block mb-2 text-sm font-medium text-white">Password</label>
-                        <input type="password" placeholder="Password" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"></input>
+                        <input type="password" placeholder="Password" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"></input>
                     </div>
-                    <button className="w-full text-white bg-lime-300 hover:bg-lime-500 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"><NavLink to="/dashboard" class="font-medium text-primary-600 hover:underline"><span className="underline pl-2 font-medium">LOGIN</span></NavLink></button>
+                    <button className="w-full text-gray-200 bg-primary  hover:bg-lime-500 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"><NavLink to="/dashboard" class="font-medium text-gray-200"><span className="no-underline pl-2 font-medium">LOGIN</span></NavLink></button>
                     <p className="text-sm font-light text-white">
-                      Don’t have an account yet?
-                      <NavLink to="/signup" className="font-medium text-primary-600 hover:underline"><span className="underline pl-2 font-medium">SIGN UP</span></NavLink>
-                  </p>
+                        Don’t have an account yet?
+                        <NavLink to="/signup" className="font-medium text-primary-600 hover:underline"><span className="underline pl-2 font-medium">SIGN UP</span></NavLink>
+                    </p>
                 </form>
             </div>
         </div>
--- my_brand/src/pages/Signup.js
+++ my_brand/src/pages/Signup.js
@@ -2,10 +2,11 @@ import { NavLink } from "react-router-dom";
 
 export default function Signup() {
     return (
-        <div className="bg-[url('https://images.unsplash.com/photo-1497864149936-d3163f0c0f4b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1169&q=80')] bg-cover">
-            <div className="flex flex-col items-center justify-center px-12 py-24 mx-auto md:h-screen lg:py-0 border-2 border-white">
-                <h1 className="block mb-2 sm:text-sm font-medium text-white text-xl "> Create your account</h1>
-                <form className="space-y-8 md:space-y-6  h-196 w-96 p-4 mt-4">
+        <div className="bg-gray-100 ">
+            <div className="flex flex-col items-center justify-center mx-auto h-screen md:h-screen lg:py-0 border-2 border-white">
+            <h1 className="flex justify-center items-center mb-6">TWC</h1>
+                <h1 className="block mb-2 sm:text-sm font-medium text-white text-xl md:text-2xl "> Create your account</h1>
+                <form className="space-y-4 md:space-y-6 h-196 w-10/12 md:w-1/2 lg:w-1/3 xl:w-1/4 p-4 mt-4 lg:p-8  shadow-2xl rounded">
                     <div className="flex flex-col">
                         <label className="block mb-2 text-sm font-medium text-white ">FirstName</label>
                         <input type="text" placeholder="email" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"></input>
@@ -22,7 +23,7 @@ export default function Signup() {
                         <label className="block mb-2 text-sm font-medium text-white">Password</label>
                         <input type="password" placeholder="Password" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"></input>
                     </div>
-                    <button className="w-full text-white bg-lime-300 hover:bg-lime-500 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">SIGN UP</button>
+                    <button className="w-full text-gray-200 bg-primary hover:bg-lime-500 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">SIGN UP</button>
                     <p className="text-sm font-light text-white">
                         Already have an account?
                         <NavLink to="/login" className="font-medium text-primary-600 hover:underline"><span className="underline pl-2 font-medium">LOGIN</span></NavLink>
